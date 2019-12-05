ecb02dbea031ac8b09b6bc73e4886ceae29b8439
agile-prototype-8421 <tylergreason@gmail.com>
2019-12-05 16:57:28 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -27,7 +27,9 @@ def find_greater_pair(src)
   rows = 0 
   
   while rows < src.count do 
+    p src[rows]
     src[rows].sort
+    p src[rows]
     new_array.push(src[rows][-1])
     rows+=1
   end
