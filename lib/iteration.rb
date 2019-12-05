3b8162087aae91d46d70c9dea04ecfaaf66e00ad
agile-prototype-8421 <tylergreason@gmail.com>
2019-12-05 16:53:09 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -10,7 +10,7 @@ def join_ingredients(src)
   new_array = []
   rows = 0 
   while rows < src.count do 
-    new_array[rows] = "I love ${src[rows][0]} and ${src[rows][1]} on my pizza"
+    new_array[rows] = "I love #{src[rows][0]} and #{src[rows][1]} on my pizza"
     rows+=1
   end
 end
